c875f09d698e5a4a5df4d4ee592b59d33fa7e9dc
Evens Antoine <evens7antoine@gmail.com>
2025-03-20 01:03:16 +0000

    Need Remove Function

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	// To-Do List
+	// it will contain a list of tasks
+	// received from user input
+	const [tasks, setTasks] = useState(["Exercise for 1 hour", "Practice more Javascript", "Journal"]);
+	const [userInput, setUserInput] = useState("");
+
+	// item = dressing
+	const addToList = (e) => {
+		e.preventDefault();
+		console.log(e)
+		setTasks([...tasks, userInput]);
+	}
+
+	const removeTask = (i) => {
+		const newArray = tasks.filter((task, index) => {}) 
+
+	}
 	return (
 		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<input value={userInput} onChange={(e) => {setUserInput(e.target.value)}}></input>
+			<button onClick={(e) => addToList(e)}>Add To List</button>
+
+			{/* we have an array of tasks and we want to display as a list on our page*/}
+			<ul>
+				{tasks?.map((task, index) => {
+					return <li key={index}>{task}</li>}
+	)}
+			</ul>
 		</div>
 	);
 };
